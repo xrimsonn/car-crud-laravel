64e3b09df90077ff5beb5e880d230db9fe10b571
Antonio Rosales <jose.rosales.mald@gmail.com>
2023-11-20 20:35:29 -0600

    refactor

--- resources/views/contact.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.home')
-
-@section('title', 'Contact us')
-
-@section('content')
-<h1>Contact us</h1>
-<p>This is our contact us page.</p>
-@endsection
--- resources/views/welcome.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.home')
-
-@section('title', 'Welcome')
-
-@section('content')
-<h1>Welcome!</h1>
-<p>This is our home page.</p>
-@endsection
